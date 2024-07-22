4620bdc89839c04fd96e71ccb834f15560cc814e
JudeTheDuud <praiseosai@gmail.com>
2024-07-23 00:15:59 +0100

    firstCommit

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function TodoCard(props) {
+  const { children, handleDeleteTodo, index, handleEditTodo } = props;
+
+  const [deleteTodos, setDeleteTodos] = useState();
+  return (
+    <li className="todoItem">
+      {children}
+      <div className="actionsContainer">
+        <button onClick={() =>{
+            handleEditTodo(index)
+        }}>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button onClick={() =>{
+            handleDeleteTodo(index)
+        }}>
+          
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default TodoCard;
